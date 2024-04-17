467f2ad7eafecf2236883496ae24a787d834fd5d
Karthikeyan Sivaraman <octotus@users.noreply.github.com>
2024-04-17 18:02:42 -0400

    Add files via upload
    Scanning for Bluetooth beacons using ESP32 involves a class definition inside which a method doing the scan is called. However, this is mostly unnecessary for simple scans and I have rewritten the code to be deployed directly in the loop of the arduino code. 
    
    This code does the following:
    1. connect to a WiFi network (you have to provide the credentials in a separate file arduino_secret.h)
    2. connect to a MQTT broker within this WiFi network (provide credentials in the same arduino_secret.h file)
    3. run periodic scans for BLE and when it finds a device, it will send a message to the MQTT broker.
    
    The MQTT broker used in this code is a simple Android based app (Narada MQTT) on default port. It has to be redefined for other brokers.
    
    The message transmitted by this scanner is a serialized JSON string, and consists of the following:
    1. Sender ID (the ESP32 Module ID). As such, if you are playing around with one scanner, this doesn't matter. But, it is also pretty useless overall. If you are using multiple scanners, make sure to change the ID. That way, you will know which scanner is sending you the message.
    2. Beacon - the BLE device that the scanner identifies. It doesn't distinguish between BLE beacons and normal BL devices; rather it can't. Therefore, the code will filter out all anonymous BL devices. Corollary for this: if you are setting up a beacon, ensure you give it a name - else this won't work. Alternatively, comment out the "if" statement.
    3. TXPower - Transmitter power at source. This is uniformly zero (0). Irrespective of the power of the transmitter, this is zero at source. I am capturing this just in case. Haven't found a use case. Maybe in future versions of the code, this will be dropped.
    4. RSSI: Received Signal Strength Intensity at the scanner. This is one of the vital pcs of info we capture. Making several assumptions about BLE transmission and environment, this number can be used to guess the distance from source.
    5. MAC Address: straightforward -- the MAC Address of the BLE Beacon. This is another universally unique way of naming your beacon: just name it to its MAC Address.

---
+++ BLE_scan_simpleNoClass.ino
@@ -0,0 +1,112 @@
+// These libraries are needed for beacon Scan
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+// These are needed for WiFi connectivity
+
+#include <WiFi.h>
+#include "ardu_secret.h"
+
+// These are needed for MQTT connection and sending MQTT messages
+#include <ArduinoMqttClient.h>
+#include <ArduinoJSON.h>
+int scanTime = 5; //In seconds
+
+char* network=SECRET_SSID;
+char* passwd=SECRET_PWD;
+
+BLEScan* pBLEScan;
+
+WiFiClient wfc;
+MqttClient mqClient(wfc);
+
+char* broker = "192.168.4.42";
+int port = 1883;
+char* topic = "BLE_beacon_details";
+
+const long interval = 5000;
+long prevMillis = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi and confirm local IP 
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(network, passwd);
+  while(WiFi.status != WL_CONNECTED)
+  {
+    delay(500);
+  }
+  Serial.print("Connected to WiFi Network: ");
+  Serial.println(network);
+  IPADDRESS ip = WiFi.localIP();
+  Serial.print("Local IP Address is: ");
+  Serial.println(ip);
+  Serial.println("\n");
+  // WiFi Connect Done
+
+  // Set up MQTT Client and Connect to Broker //
+  mqClient.setId("ESP3201");
+  // use this space to set username / password for accessing the server //
+  //mqClient.setUsernamePassword(str user, str psswd);
+
+  if(!mqClient.connect(broker,port))
+  {
+    Serial.println("Trying to connect to broker");
+    while(1);
+  }
+  
+  Serial.println("Connected to broker");
+  
+  //Set up BLE Scan //
+  Serial.println("Scanning...");
+
+  BLEDevice::init("ESP3201");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  //pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // MQTT Part
+
+  mqClient.poll();
+
+  long curr_millis = millis();
+  long diff = curr_millis - prevMillis;
+
+  if(diff > interval)
+  {
+    BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+    Serial.print("Devices found: ");
+    Serial.println(foundDevices.getCount());
+    int count = foundDevices.getCount();
+    for(int i = 0; i < count; i++)
+    {
+      BLEAdvertisedDevice device=(foundDevices.getDevice(i));
+      std::string dName = device.getName();
+      int dTxPower = device.getTXPower();
+      int rssi = device.getRSSI();
+      String address = device.getAddress().toString().c_str();
+      if(dName.size() > 0)
+      {
+        Serial.print(dName.c_str());
+        Serial.print("\t"); Serial.print(address); 
+        Serial.print("\t"); Serial.print(dTxPower);
+        Serial.print("\t"); Serial.println(rssi);
+
+        JsonDocument message;
+        message[""] = 
+        
+      }
+    }
+     Serial.println("Scan done!");
+    pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+}
