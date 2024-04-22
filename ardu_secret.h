1b0a92eb0ec78bb8a654f69ec3e2d748afaeb6d8
octotus <karthi.sivaraman@gmail.com>
2024-04-21 20:00:57 -0400

    The header ardu_secret.h file now contains necessary details: 1) wifi ssid, 2) wifi password, 3) broker user, 4) broker password, 5) broker IP, 6) broker port and 7) DEVICE (device name). These details are now captured in a data structure. The struct is defined in the header as well. This version of the ardu_secret.h has two variables of the data structure, to be filled. This information will be used in the current version of BLEScan_MQTTClienct.ino to sequentially try and connect to one of the two networks / brokers. If the first one (default) fails, then the code will automatically try to connect to the second / backup network.

--- ardu_secret.h
+++ ardu_secret.h
@@ -1,13 +1,30 @@
-
-// NETWORK NAME AND PASSWD
-
-#define SECRET_SSID <SSID HERE>
-#define SECRET_PWD <PASSWD HERE>
-
-// BROKER USER / PASSWD
-
-#define BROKER_USER <BROKER NAME HERE>
-#define BROKER_PWD <BROKER PWD HERE>
-
 // DEVICE ID -- change for every unit
-#define DEVICE <DEVICE NAME HERE>
+
+typedef struct connect_data{
+  char* network;
+  char* net_passwd;
+  char* broker;
+  char* broker_pwd;
+  char* broker_IP;
+  int port;
+} connect_data;
+
+connect_data c1 = {
+  .network = <SSID1>,
+  .net_passwd = <PASSWD1>,
+  .broker = <BROKER_UNAME1>,
+  .broker_pwd = <BROKER_PWD1>,
+  .broker_IP = <BROKER_IP1>,
+  .port = <BROKER_PORT1>
+};
+
+connect_data c2 = {
+  .network = <SSID2>
+  .net_passwd = <PASSWD2>,
+  .broker = <BROKER_UNAME2>,
+  .broker_pwd = <BROKER_PWD2>,
+  .broker_IP = <BROKER_IP2>,
+  .port = <BROKER_PORT2>
+};
+#define DEVICE "ESP3203"
+
