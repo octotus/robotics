ba7e18f72af19955ef828c374f36814f2f237aa3
octotus <karthi.sivaraman@gmail.com>
2024-04-27 22:57:35 -0400

    details on network now stored in a linked list struct. there is a function that is called from setup returning the linked list pointer. This allows multiple network / MQTT server options to be specified, and iterated in an order of preference automatically. The iteration breaks when a successful connection is made.

--- ardu_secret.h
+++ ardu_secret.h
@@ -1,30 +1,39 @@
 // DEVICE ID -- change for every unit
 
 typedef struct connect_data{
-  char* network;
-  char* net_passwd;
-  char* broker;
-  char* broker_pwd;
-  char* broker_IP;
-  int port;
+  char* network; // Netwokr SSID
+  char* net_passwd; // password
+  char* broker; // broker user ID
+  char* broker_pwd; // broker PWD
+  char* broker_IP; // broker IP
+  int port; // port - default = 1883
+  struct connect_data *next; //points to subsequent options; NULL else.
 } connect_data;
 
-connect_data c1 = {
-  .network = <SSID1>,
-  .net_passwd = <PASSWD1>,
-  .broker = <BROKER_UNAME1>,
-  .broker_pwd = <BROKER_PWD1>,
-  .broker_IP = <BROKER_IP1>,
-  .port = <BROKER_PORT1>
-};
 
-connect_data c2 = {
-  .network = <SSID2>
-  .net_passwd = <PASSWD2>,
-  .broker = <BROKER_UNAME2>,
-  .broker_pwd = <BROKER_PWD2>,
-  .broker_IP = <BROKER_IP2>,
-  .port = <BROKER_PORT2>
-};
+connect_data* define_linked_list()
+{
+  connect_data *head = NULL;
+  connect_data *c1 = (connect_data *)malloc(sizeof(connect_data));
+  connect_data *c2 = (connect_data *)malloc(sizeof(connect_data));
+
+
+  c1->network = "";
+  c1->net_passwd = "";
+  c1->broker = "";
+  c1->broker_pwd = "";
+  c1->broker_IP = "";
+  c1->port = 1883;
+  c1->next = c2;
+ /* 
+  c2->network = "";
+  c2->net_passwd = "
+  c2->broker = "";
+  c2->broker_pwd = "
+  c2->broker_IP = "";
+  c2->port = 1883;
+  c2->next = NULL; */
+  head = c1;
+  return head;
+}
 #define DEVICE "ESP3203"
-
