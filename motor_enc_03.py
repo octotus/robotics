4714f28bebe6aa35e93bba53aae1b0eb781ca64d
octotus <karthi.sivaraman@gmail.com>
2024-06-01 12:01:15 -0400

    does a moderate error correction post run completion. Lots of drift due to motor speed differences - may have to provide different PWM values for the vehicle. Also, the errors are variable.

--- motor_enc_03.py
+++ motor_enc_03.py
@@ -2,9 +2,9 @@ import RPi.GPIO as gpio
 import time
 from math import pi
 
-
-
 gpio.setmode(gpio.BCM)
+
+fwd_flag = True
 
 circ = 2 * pi * 5
 
@@ -18,7 +18,6 @@ m1_A = 25
 m1_B = 23
 m2_A = 10
 m2_B = 9
-
 
 for i in output_pins.keys():
     pin = output_pins[i];
@@ -36,8 +35,9 @@ gpio.setup(m2_B,gpio.IN,pull_up_down = gpio.PUD_DOWN)
 
 
 counter_dict={}
-counter_dict[1]=0;
-counter_dict[2]=0;
+def reset_counter():
+    counter_dict[1]=0;
+    counter_dict[2]=0;
 
 rotation={}
 rotation[1]=0
@@ -67,10 +67,26 @@ def move(speed):
     pwm1.start(speed)
     pwm2.start(speed)
 
+def correct_diff(motor,clicks):
+    stop()
+    time.sleep(0.2)
+    if(motor == 1):
+        dir_pin = output_pins['dir1'];
+        pwm = pwm1
+    else:
+        dir_pin = output_pins['dir2'];
+        pwm = pwm2
+    while(counter_dict[motor] < clicks):
+        gpio.output(dir_pin,gpio.HIGH)
+        pwm.start(20);
+    stop()
+
+
 def stop():
     pwm1.stop()
     pwm2.stop()
 
+reset_counter()
 
 move(40)
 
@@ -83,6 +99,30 @@ for i in range(0,10):
     diff = dist1 - dist2
     pce = 100* diff / dist1
     print(f'{rotation[1]:.3f}\t{rotation[2]:.3f}\t{counter_dict[1]}\t{counter_dict[2]}\t{dist1:.3f}\t{dist2:.3f}\t{diff:.3f}\t{pce:.3f}')
+
+stop()
+
+time.sleep(2)
+
+tot_click1 = (rotation[1] * 3316) + counter_dict[1]
+tot_click2 = (rotation[2] * 3316) + counter_dict[2]
+
+diff_click = tot_click1 - tot_click2
+
+if(diff_click < 0):
+    # motor 2 has moved more than motor 1 - move motor1 to adjust
+    reset_counter()
+    motor = 2
+    print(f'motor ID: {motor}\tDifference: {diff_click}')
+    time.sleep(1)
+    correct_diff(motor,abs(diff_click))
+else:
+    reset_counter()
+    motor = 1
+    print(f'motor ID: {motor}\tDifference: {diff_click}')
+    time.sleep(1)
+    correct_diff(motor,abs(diff_click))
+
 
 stop()
 gpio.cleanup()
